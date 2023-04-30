9ddffb7ede89becd17039dcb17d9b14b3ea3da8e
YashBharambay <ybharambay@gmail.com>
2023-04-30 00:56:45 -0400

    WIP: Client-Side Create Listing

--- client/src/pages/CreateListing/index.jsx
+++ client/src/pages/CreateListing/index.jsx
@@ -1,17 +1,162 @@
 import PlacesAutocomplete from 'components/PlacesAutocomplete';
-import React from 'react';
+import React, { useState } from 'react';
 import { getLocationDetails } from 'utils/helpers';
+import { Box, Button, TextField, FormControl } from '@mui/material';
+import { POST, handleError } from 'utils/api-calls';
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import moment from 'moment';
+import { useNavigate } from 'react-router-dom';
+import { errorAlert } from 'store/alert';
+import { useDispatch } from 'react-redux';
 
 function CreateListing() {
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const [formValues, setFormValues] = useState({
+		apt: '',
+		description: '',
+		bedrooms: '',
+		bathrooms: '',
+		rent: '',
+		deposit: '',
+		availabilityDate: null,
+		location: {},
+	});
+
+	const [isDisabled, setIsDisabled] = useState(false);
+
+	const handleChange = (event) => {
+		const { name, value, type } = event.target;
+
+		setFormValues({
+			...formValues,
+			[name]: type === 'number' ? parseInt(value, 10) : value,
+		});
+	};
+
+	const handleSubmit = async (event) => {
+		try {
+			setIsDisabled(true);
+			event.preventDefault();
+			const res = await POST('/listings', formValues);
+			if (res && res.listing._id) {
+				navigate(`/listings/${res.listing._id}`);
+			}
+		} catch (e) {
+			let error = 'Unexpected error occurred';
+			if (typeof handleError(e) === 'string') error = handleError(e);
+			dispatch(errorAlert(error));
+		}
+	};
+
 	return (
-		<div>
-			<PlacesAutocomplete
-				onChange={async (location) => {
-					const formattedLocation = await getLocationDetails(location);
-					console.log(formattedLocation);
+		<Box>
+			<Box
+				sx={{
+					minHeight: '60vh',
+					display: 'flex',
+					flexDirection: 'column',
+					alignItems: 'center',
+					justifyContent: 'center',
+					gap: 4,
 				}}
-			/>
-		</div>
+			>
+				<form onSubmit={handleSubmit}>
+					<Box
+						sx={{
+							display: 'flex',
+							flexDirection: 'column',
+							alignItems: 'center',
+							justifyContent: 'center',
+							gap: 2,
+							minWidth: '500px',
+						}}
+					>
+						<TextField
+							required
+							name="apt"
+							type="number"
+							label="Apartment Number"
+							value={formValues.apt}
+							onChange={handleChange}
+						/>
+						<TextField
+							required
+							name="description"
+							label="Description"
+							value={formValues.description}
+							onChange={handleChange}
+						/>
+						<TextField
+							required
+							type="number"
+							name="bedrooms"
+							label="Bedrooms"
+							value={formValues.bedrooms}
+							onChange={handleChange}
+						/>
+						<TextField
+							required
+							type="number"
+							name="bathrooms"
+							label="Bathrooms"
+							value={formValues.bathrooms}
+							onChange={handleChange}
+						/>
+						<TextField
+							required
+							type="number"
+							name="rent"
+							label="Rent"
+							value={formValues.rent}
+							onChange={handleChange}
+						/>
+						<TextField
+							required
+							type="number"
+							name="deposit"
+							label="Deposit"
+							value={formValues.deposit}
+							onChange={handleChange}
+						/>
+						<FormControl fullWidth>
+							<LocalizationProvider dateAdapter={AdapterMoment}>
+								<DatePicker
+									label="Availability Date"
+									value={formValues.availabilityDate}
+									format="MM-DD-YYYY"
+									minDate={moment()}
+									onChange={(newValue) => {
+										setFormValues({
+											...formValues,
+											availabilityDate: newValue
+												? newValue.format('MM-DD-YYYY')
+												: null,
+										});
+									}}
+								/>
+							</LocalizationProvider>
+						</FormControl>
+						<PlacesAutocomplete
+							name="location"
+							onChange={async (location) => {
+								const formattedLocation = await getLocationDetails(location);
+								formValues.location = formattedLocation;
+							}}
+						/>
+						<Button
+							variant="contained"
+							color="primary"
+							type="submit"
+							disabled={isDisabled}
+						>
+							Submit
+						</Button>
+					</Box>
+				</form>
+			</Box>
+		</Box>
 	);
 }
 
