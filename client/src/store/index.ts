8cb971a0db823289bdd9d61b92a797fb412e3c12
Abhishek Khatri <abhishekkhatri98@gmail.com>
2023-05-04 19:42:26 -0400

    feat: custom useSelector and useDispatch hooks with types

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { useDispatch } from 'react-redux';
 import alert from './alert';
 import user from './user';
 import app from './app';
@@ -11,6 +10,3 @@ export default store;
 // state and dispatch types
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
-// custom useDispatch hook with AppDispatch type
-export const useAppDispatch: () => AppDispatch = useDispatch;
