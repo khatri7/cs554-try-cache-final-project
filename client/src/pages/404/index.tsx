4f2519093e0b29724f3972023190f1d13f17487a
Abhishek Khatri <abhishekkhatri98@gmail.com>
2023-05-09 18:23:33 -0400

    feat: added more scss

--- client/src/pages/404/index.tsx
+++ client/src/pages/404/index.tsx
@@ -2,6 +2,7 @@ import { Button } from '@mui/material';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import image from './404A.gif';
+import './404.scss';
 
 const ErrorPage: React.FC<{}> = () => {
 	const navigate = useNavigate();
@@ -10,23 +11,8 @@ const ErrorPage: React.FC<{}> = () => {
 	};
 	return (
 		<div className="notFound__container">
-			<div
-				style={{
-					position: 'fixed',
-					inset: 0,
-					backgroundColor: '#f0f0f0',
-					zIndex: -1,
-				}}
-			/>
-			<div
-				className="notFound__content"
-				style={{
-					display: 'flex',
-					justifyContent: 'center',
-					alignItems: 'center',
-					flexDirection: 'column',
-				}}
-			>
+			<div className="notFound__background" />
+			<div className="notFound__content">
 				<img src={image} alt="404" style={{ width: '50%' }} />
 				<Button
 					type="button"
